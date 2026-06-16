693a05cb1f5634535bbd84db06f6689aab7b7e2d
Ngoc <joycengo.nguyenngoc@gmail.com>
2026-06-16 19:03:54 +0900

    update inc setup

--- inc/setup.php
+++ inc/setup.php
@@ -21,21 +21,53 @@ add_action(
     'lefoin_setup'
 );
 
+
 function lefoin_assets(){
 
     wp_enqueue_style(
-
         'lefoin-style',
-
         get_stylesheet_uri(),
-
         [],
-
         '1.0'
+    );
 
+    wp_enqueue_style(
+        'lefoin-framework',
+        get_template_directory_uri() .
+        '/assets/css/lefoin.css',
+        [],
+        '1.0'
+    );
+
+    wp_enqueue_script(
+        'lefoin-js',
+        get_template_directory_uri() .
+        '/assets/js/lefoin.js',
+        [],
+        '1.0',
+        true
+    );
+
+    wp_enqueue_script(
+        'auth-js',
+        get_template_directory_uri() .
+        '/assets/js/auth.js',
+        [],
+        '1.0',
+        true
+    );
+
+    wp_enqueue_script(
+        'wallet-js',
+        get_template_directory_uri() .
+        '/assets/js/wallet.js',
+        [],
+        '1.0',
+        true
     );
 
 }
+
 
 add_action(
 
